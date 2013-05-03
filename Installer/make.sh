dc4567bfcf5192da5f159f61eacdca334dc05d9a
Eric Woudenberg <eaw@woudy.org>
2013-05-03 11:57:38 +0000

    Modify Installer Maker to take a version number argument.

--- Installer/make.sh
+++ Installer/make.sh
@@ -1,6 +1,15 @@
-echo Current config file: 
-cat ../GGC_FlightSheet/bin/Debug/FlightSheetSettings.xml
+if [ $# -lt 1 ]
+then
+  echo 'usage: make.sh <version> (e.g. 1.0.5)'
+  exit
+fi
+version="$1"
+config="../GGC_FlightSheet/bin/Debug/FlightSheetSettings.xml"
+
+echo Current config file: $config
+cat $config
 echo
 echo -n cr to proceed:
 read a
 makensis BuildGFS.nsi
+mv GGC_FlightSheetsInstaller.exe "GGC_FlightSheetsInstaller_$version.exe"
